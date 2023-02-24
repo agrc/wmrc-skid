9ac6d17f54ee491adc091f6ce96987b78d311932
Jake Adams <jdadams@utah.gov>
2023-02-23 21:00:51 -0700

    chore: setup and config info

--- src/wmrc/config.py
+++ src/wmrc/config.py
@@ -7,19 +7,18 @@ import socket
 
 SKID_NAME = 'wmrc'
 
-AGOL_ORG = 'https://utah.maps.arcgis.com'
+AGOL_ORG = 'https://utahdeq.maps.arcgis.com'
 SENDGRID_SETTINGS = {  #: Settings for SendGridHandler
     'from_address': 'noreply@utah.gov',
-    'to_addresses': '',
+    'to_addresses': 'jdadams@utah.gov',
     'prefix': f'{SKID_NAME} on {socket.gethostname()}: ',
 }
 LOG_LEVEL = logging.DEBUG
 LOG_FILE_NAME = 'log'
 
-FEATURE_LAYER_ITEMID = ''
-JOIN_COLUMN = ''
+FEATURE_LAYER_ITEMID = '4df06137fb0a45459e49107a5f47a326'  #: Beta version
+# FEATURE_LAYER_ITEMID = '056bbc52ff3240f6b69666750a61aeff'  #: Live version
+JOIN_COLUMN = 'id_'
 ATTACHMENT_LINK_COLUMN = ''
 ATTACHMENT_PATH_COLUMN = ''
-FIELDS = {
-
-}
+FIELDS = {}

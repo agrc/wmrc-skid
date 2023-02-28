129653acd1ae85875ba93d040c16f22b09b90cb8
Jake Adams <jdadams@utah.gov>
2023-02-28 13:48:21 -0700

    chore: correct test for google auth

--- tests/test_skidname.py
+++ tests/test_skidname.py
@@ -4,10 +4,11 @@ from wmrc import main
 def test_get_secrets_from_gcp_location(mocker):
     mocker.patch('pathlib.Path.exists', return_value=True)
     mocker.patch('pathlib.Path.read_text', return_value='{"foo":"bar"}')
+    mocker.patch('google.auth.default', return_value=('sa', 42))
 
     secrets = main._get_secrets()
 
-    assert secrets == {'foo': 'bar'}
+    assert secrets == {'foo': 'bar', 'SERVICE_ACCOUNT_JSON': 'sa'}
 
 
 def test_get_secrets_from_local_location(mocker):

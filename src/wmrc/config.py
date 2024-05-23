4594635180e0a8ff8c64d837e4eb4f5d9075231a
Jake Adams <jdadams@utah.gov>
2024-05-23 08:21:26 -0600

    chore: double qoutes

--- src/wmrc/config.py
+++ src/wmrc/config.py
@@ -6,12 +6,12 @@ import logging
 import socket
 import urllib.request
 
-SKID_NAME = 'wmrc'
+SKID_NAME = "wmrc"
 #: Try to get project id from GCP metadata server for hostname. If it's empty or errors out, revert to local hostname
 try:
-    url = 'http://metadata.google.internal/computeMetadata/v1/project/project-id'
+    url = "http://metadata.google.internal/computeMetadata/v1/project/project-id"
     req = urllib.request.Request(url)
-    req.add_header('Metadata-Flavor', 'Google')
+    req.add_header("Metadata-Flavor", "Google")
     project_id = urllib.request.urlopen(req).read().decode()
     if not project_id:
         raise ValueError
@@ -19,17 +19,17 @@ try:
 except Exception:
     HOST_NAME = socket.gethostname()
 
-AGOL_ORG = 'https://utahdeq.maps.arcgis.com'
+AGOL_ORG = "https://utahdeq.maps.arcgis.com"
 SENDGRID_SETTINGS = {  #: Settings for SendGridHandler
-    'from_address': 'noreply@utah.gov',
-    'to_addresses': ['jdadams@utah.gov', 'stevienorcross@utah.gov', 'gerardorodriguez@utah.gov'],
-    'prefix': f'{SKID_NAME} on {HOST_NAME}: ',
+    "from_address": "noreply@utah.gov",
+    "to_addresses": ["jdadams@utah.gov", "stevienorcross@utah.gov", "gerardorodriguez@utah.gov"],
+    "prefix": f"{SKID_NAME} on {HOST_NAME}: ",
 }
 LOG_LEVEL = logging.DEBUG
-LOG_FILE_NAME = 'log'
+LOG_FILE_NAME = "log"
 
-COUNTIES_ITEMID = '90431cac2f9f49f4bcf1505419583753'
+COUNTIES_ITEMID = "90431cac2f9f49f4bcf1505419583753"
 
 # FEATURE_LAYER_ITEMID = '4df06137fb0a45459e49107a5f47a326'  #: Beta version
-FEATURE_LAYER_ITEMID = '056bbc52ff3240f6b69666750a61aeff'  #: Live version
-JOIN_COLUMN = 'id_'
+FEATURE_LAYER_ITEMID = "056bbc52ff3240f6b69666750a61aeff"  #: Live version
+JOIN_COLUMN = "id_"

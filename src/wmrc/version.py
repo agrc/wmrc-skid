7d2333203aa08f831131f85b02824b2e76336c02
Jake Adams <jdadams@utah.gov>
2024-08-16 12:16:46 -0600

    ci: refactor actions
    Co-authored-by: Scott Davis <stdavis@utah.gov>

--- src/wmrc/version.py
+++ src/wmrc/version.py
@@ -1,6 +1,6 @@
 """A single source of truth for the version in a programatically-accessible variable.
 
-This must only include a single line: __version__ = 'x.y.z'
+This must only include a single line setting the dunder version variable.
 """
 
 __version__ = "1.2.0"
